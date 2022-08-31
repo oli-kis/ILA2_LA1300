8507d0aa0bcf64455f96d22523f27342dd7bc913
oli-kis <89086304+oli-kis@users.noreply.github.com>
2022-08-31 10:09:49 +0200

    Add files via upload

---
+++ Files/LA_1300_NumberGuesserGUI/LA_1300_NumberGuesserGUI/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace LA_1300_NumberGuesserGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbxNumber = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.lblMSG = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblGuessCounter = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbxNumber
+            // 
+            this.tbxNumber.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tbxNumber.BackColor = System.Drawing.Color.DarkViolet;
+            this.tbxNumber.Font = new System.Drawing.Font("Bahnschrift Condensed", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxNumber.ForeColor = System.Drawing.Color.White;
+            this.tbxNumber.Location = new System.Drawing.Point(151, 404);
+            this.tbxNumber.Name = "tbxNumber";
+            this.tbxNumber.Size = new System.Drawing.Size(200, 56);
+            this.tbxNumber.TabIndex = 0;
+            this.tbxNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbxNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbxNumber_KeyPress);
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnGuess.BackColor = System.Drawing.Color.BlueViolet;
+            this.btnGuess.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnGuess.Font = new System.Drawing.Font("Bahnschrift Condensed", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuess.ForeColor = System.Drawing.Color.White;
+            this.btnGuess.Location = new System.Drawing.Point(382, 392);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(274, 68);
+            this.btnGuess.TabIndex = 1;
+            this.btnGuess.Text = "RATEN";
+            this.btnGuess.UseVisualStyleBackColor = false;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // lblMSG
+            // 
+            this.lblMSG.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblMSG.BackColor = System.Drawing.Color.Fuchsia;
+            this.lblMSG.Font = new System.Drawing.Font("Bahnschrift Condensed", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMSG.ForeColor = System.Drawing.Color.White;
+            this.lblMSG.Location = new System.Drawing.Point(155, 217);
+            this.lblMSG.Name = "lblMSG";
+            this.lblMSG.Size = new System.Drawing.Size(738, 136);
+            this.lblMSG.TabIndex = 3;
+            this.lblMSG.Text = "GEBEN SIE EINE ZAHL ZWISCHEN 1-100 EIN";
+            this.lblMSG.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1039, 81);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.AutoSize = false;
+            this.menuToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.menuToolStripMenuItem.Font = new System.Drawing.Font("Bahnschrift Condensed", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(250, 75);
+            this.menuToolStripMenuItem.Text = "PLAY AGAIN!";
+            this.menuToolStripMenuItem.Click += new System.EventHandler(this.menuToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.AutoSize = false;
+            this.toolStripMenuItem1.Font = new System.Drawing.Font("Bahnschrift Condensed", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripMenuItem1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(200, 77);
+            this.toolStripMenuItem1.Text = "EXIT";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1039, 652);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblGuessCounter
+            // 
+            this.lblGuessCounter.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblGuessCounter.BackColor = System.Drawing.Color.Magenta;
+            this.lblGuessCounter.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblGuessCounter.Font = new System.Drawing.Font("Bahnschrift Condensed", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuessCounter.ForeColor = System.Drawing.Color.White;
+            this.lblGuessCounter.Location = new System.Drawing.Point(681, 404);
+            this.lblGuessCounter.Name = "lblGuessCounter";
+            this.lblGuessCounter.Size = new System.Drawing.Size(212, 56);
+            this.lblGuessCounter.TabIndex = 6;
+            this.lblGuessCounter.Text = "VERSUCHE: 0";
+            this.lblGuessCounter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.checkBox1.BackColor = System.Drawing.Color.BlueViolet;
+            this.checkBox1.Font = new System.Drawing.Font("Bahnschrift Condensed", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(382, 493);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.checkBox1.Size = new System.Drawing.Size(274, 58);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "MUSIK SPIELEN";
+            this.checkBox1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1039, 652);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.lblMSG);
+            this.Controls.Add(this.lblGuessCounter);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.tbxNumber);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "NumberGuesser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxNumber;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Label lblMSG;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblGuessCounter;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Files/LA_1300_NumberGuesserGUI/LA_1300_NumberGuesserGUI/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace LA_1300_NumberGuesserGUI
+{
+    public partial class Form1 : Form
+    {
+
+        Random random = new Random();
+        int secretNumber;
+        int guessCounter = 0;
+        SoundPlayer player = new SoundPlayer(@"C:\Users\olivier\OneDrive - Alte Kantonsschule Aarau\Dokumente\BBBaden\Lernatelier Schneider\LA_1300\LA_1300_NumberGuesserGUI\LA_1300_NumberGuesserGUI\MonteWAV.wav");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            secretNumber = random.Next(1, 101);
+            player.Load();
+        }
+
+        private void tbxNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(!(char.IsDigit(e.KeyChar)))
+            {
+                e.Handled = true;
+            }
+
+            if (e.KeyChar == (char)Keys.Back)
+            {
+                e.Handled = false;
+            }
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            int guess = 0;
+            if(!(int.TryParse(tbxNumber.Text, out guess)))
+            {
+                lblMSG.Text = "Sie können die Textbox nicht leer lassen";
+                return; 
+            }
+
+            if (guess < 1 || guess > 100)
+            {
+                lblMSG.Text = "Geben Sie eine Zahl zwischen 1-100 ein";
+                tbxNumber.Text = "";
+                return;
+            }
+
+            if (guess < secretNumber)
+            {
+                lblMSG.Text = "Die gesuchte Zahl ist grösser";
+                tbxNumber.Text = "";
+            }
+
+            if (guess > secretNumber)
+            {
+                lblMSG.Text = "Die gesuchte Zahl ist kleiner";
+                tbxNumber.Text = "";
+            }
+
+            if (guess == secretNumber)
+            {
+                lblMSG.Text = "Gratulation Sie haben die Zahl erraten, bitte wählen Sie erneut spielen.";
+                tbxNumber.ReadOnly = true;
+                btnGuess.Enabled = false;
+            }
+
+            guessCounter++;
+            lblGuessCounter.Text = "Versuche: " + guessCounter;
+        }
+
+
+
+        private void menuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            secretNumber = random.Next(1, 101);
+            btnGuess.Enabled = true;
+            tbxNumber.ReadOnly = false;
+            lblMSG.Text = "Geben Sie eine Zahl zwischen 1-100 ein";
+            tbxNumber.Text = "";
+            guessCounter = 0;
+            lblGuessCounter.Text = "Versuche: 0";
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                player.Play();
+            }
+            else
+            {
+                player.Stop();
+            }
+        }
+    }
+}
